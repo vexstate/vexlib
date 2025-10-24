0549560156e91fdd708244b667ec90ee1b0af2ee
м <174154109+n11kol11c@users.noreply.github.com>
2025-10-25 01:57:32 +0200

    Refactor fxmanifest.lua for better organization
    Updated fxmanifest to use new structure and exports.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,19 +1,22 @@
 fx_version 'cerulean'
 game 'gta5'
-author 'Matija && Vexstate'
-description 'VexLib - Clean Lua library for FiveM (ESX and OX compatible)'
+author 'Matija && Vextstate'
+description 'VexLib - Clean Lua Framework for FiveM (ESX and OX compatible)'
 version '2.4.5'
 
-shared_script 'shared/vex_shared.lua'
-shared_script 'shared/exceptions.lua'
-shared_script 'shared/locale.lua'
+shared_script {
+    'shared/vex_shared.lua',
+    'shared/exceptions.lua',
+    'shared/locale.lua'
+}
 
-server_script 'server/vex_server.lua'
-server_script 'server/esx_integration.lua'
+exports {
+    'import'
+}
+
+server_script {
+    'server/vex_server.lua',
+    'server/esx_integration.lua'
+}
+
 client_script 'client/vex_client.lua'
-
-
-server_export 'getVersion'
-server_export 'getExceptionDefinitions'
-server_export 'getConfig'
-server_export 'import'
