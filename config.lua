d5035e9a851441da48f701974abba8c11d05577a
м <174154109+n11kol11c@users.noreply.github.com>
2025-10-24 21:49:02 +0200

    Created new tables and inter with ox_inventory

--- config.lua
+++ config.lua
@@ -1,7 +1,13 @@
 Config = {}
+Utils = {}
+World = {}
+Blip = {}
+Player = {}
+Exceptions = {}
 
 Config.Version = '2.4.5'
 Config.Name = 'Vexlib'
 Config.Locale = 'en'
 Config.Debug = false
+Config.Inventory = 'OX' -- or ESX
 Config.MaxPlayersSync = 48 -- 32
