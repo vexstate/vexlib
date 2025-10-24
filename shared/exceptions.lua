cd3ebfa932e255c2afe0ef0e304b992a9f0c6e2e
м <174154109+n11kol11c@users.noreply.github.com>
2025-10-25 01:59:06 +0200

    Refactor Exceptions module with humane functions

--- shared/exceptions.lua
+++ shared/exceptions.lua
@@ -1,3 +1,5 @@
+local Exceptions = Vex.Exceptions
+
 local base = {
     __index = function(t,k) return rawget(t,k) end
 }
@@ -13,47 +15,60 @@ end
 Exceptions.OK = {
     Type = 'Exceptions_OK',
     Locale = 'OK',
-    Humane = 'If everything is right, return right'
+    Humane = function(lang)
+        return Vex.Locale.get('OK',
+        lang or Vex.Config.Locale)
+    end
 }
 
 Exceptions.Exception = {
     Type = 'Exceptions_Exception',
-    Locale = 'Exception'
+    Locale = "Exception",
+    Humane = function (lang)
+        return Vex.Locale.get('Exception',
+        lang or Vex.Config.Locale)
+    end
 }
-
-Exceptions.InvalidTypeException = {
+Exceptions.InvalidType = {
     Type = 'Exceptions_InvalidTypeException',
-    Locale = 'InvalidTypeException'
+    Locale = 'InvalidType',
+    Humane = function (lang)
+        return Vex.Locale.get('InvalidType',
+        lang or Vex.Config.Locale)
+    end
 }
 
-Exceptions.InvalidValueError = {
-    Type = 'Exceptions_InvalidValueError',
-    Locale = 'InvalidValueError'
+Exceptions.InvalidValue = {
+    Type = 'Exceptions_InvalidValue',
+    Locale = 'InvalidValue',
+    Humane = function (lang)
+        return Vex.Locale.get('InvalidValue',
+        lang or Vex.Config.Locale)
+    end
 }
 
-Exceptions.InvalidArgumentError = {
-    Type = 'Exceptions_InvalidArgumentError',
-    Locale = 'InvalidArgumentError',
-    Humane = 'Got an invalid position argument'
-}
-
-Exceptions.TargetNotFoundError = {
-
+Exceptions.InvalidArgument = {
+    Type = 'Exceptions_InvalidArgument',
+    Locale = 'InvalidArgument',
+    Humane = function (lang)
+        return Vex.Locale.get('InvalidArgument',
+        lang or Vex.Config.Locale)
+    end
 }
 
 function Vex.Throw(exceptionDef, details)
     local ex = Exceptions.new(
     exceptionDef.Type or 'Exceptions_Exception',
-    exceptionDef.Locale or 'Exception', details)
+    exceptionDef.Locale or 'Exception', details or '')
 
-    local dt = details or ex.Humane
+    local dt = details
 
     local msg
     if Vex.Locale and Vex.Locale.get then
         msg = Vex.Locale.get(ex.Locale) or
-        ('['..ex.Type..'] ' .. tostring(dt))
+        ('['..ex.Type..'] ' .. tostring(dt or ''))
     else
-        msg = '['..ex.Type..'] ' .. tostring(dt)
+        msg = '['..ex.Type..'] ' .. tostring(dt or '')
     end
 
     error(msg, 2)
