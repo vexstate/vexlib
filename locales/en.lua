1e2fee86bf88fe4dd23a3c8cfc7c0bb4167ac7dd
м <174154109+n11kol11c@users.noreply.github.com>
2025-10-28 12:11:15 +0100

    Update en.lua
    Added new translations and debug states

--- locales/en.lua
+++ locales/en.lua
@@ -1,11 +1,14 @@
 Locales = Locales or {}
+
 Locales['en'] = {
     ['welcome'] = 'Welcome to Vexstate!',
     ['goodbye'] = 'See you later!',
     ['Exception'] = 'An unknown error occurred.',
     ['InvalidType'] = 'Invalid type provided.',
     ['InvalidValue'] = 'Invalid value provided.',
-    ['OK'] = 'Everything is right',
+    ['OK_CAPS'] = 'Everything is right',
+    ['OK'] = 'ok',
+    ['ERROR'] = 'error',
     ['InvalidArgument'] = 'Invalid argument provided.'
 }
 
