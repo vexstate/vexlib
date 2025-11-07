03f7b22f07fe893b06c9fe2276b5cd120e756659
м <174154109+n11kol11c@users.noreply.github.com>
2025-11-07 18:17:22 +0100

    Create label.lua

---
+++ client/label/label.lua
@@ -0,0 +1,12 @@
+Labels = Labels or {}
+
+Labels['visual'] = {
+    ['break'] = '~n~',
+    ['bold'] = '~h~',
+    ['italic'] = '~italic~',
+    ['wanted_star'] = '~ws~',
+    ['condensed_open'] = '<C>',
+    ['condensed_close'] = '</C>',
+    ['noreturn'] = '~nrt~',
+    ['R*'] = '~EX_R*~'
+}
