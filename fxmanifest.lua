9fa25cc11d01ba7f8b2bd8a67fa9190820ece40f
м <174154109+n11kol11c@users.noreply.github.com>
2025-11-16 12:48:06 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,36 +2,3 @@ fx_version 'cerulean'
 game 'gta5'
 author 'Matija && Vextstate'
 description 'Vexlib - Clean Lua Framework for FiveM (ESX and OX compatible)'
-version '2.4.5'
-
-server_exports {
-    'proxy',
-    'global_t'
-}
-
-client_exports {
-    'client_t'
-}
-
-server_scripts {
-    'config.lua',
-    'server/vex_server.lua',
-    'shared/locale.lua',
-    'locales/en.lua',
-    'locales/es.lua',
-    'shared/vex_shared.lua',
-    'shared/exceptions.lua',
-    'server/esx_integration.lua',
-}
-
-client_scripts {
-    'client/vex_client.lua',
-    'client/label/label.lua',
-    'client/locales/en.lua'
-}
-
--- old proxy
--- exports {
---     'proxy', 'global_t', 'client_t'
--- }
--- 
