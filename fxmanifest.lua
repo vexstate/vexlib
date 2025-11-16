5eb4b3676d342c4606909b6484190327d8c67bc2
м <174154109+n11kol11c@users.noreply.github.com>
2025-11-16 19:42:28 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,28 +5,28 @@ author 'Matija && Vextstate'
 description 'Vexlib - Clean Lua Framework for FiveM (ESX and OX compatible)'
 version '3.0.0'
 
+
 shared_scripts {
     'shared/init.lua',
     'shared/modules/alias.lua',
-    'shared/modules/format.lua',
-    'shared/modules/exceptions.lua',
     'shared/modules/locale.lua',
-    'shared/modules/table.lua',
-    'shared/modules/str.lua',
-    'shared/modules/utils.lua'
+    'shared/modules/format.lua',
+    'shared/modules/utils.lua',
+    'shared/modules/exceptions.lua'
 }
 
+server_scripts {
+    'server/main.lua',
+    'server/commands.lua',
+    'server/esx_integration.lua'
+}
 
 client_scripts {
     'client/client.lua',
-    'client/modules/ui.lua'
+    'locales/lua/en.lua'
 }
 
-
-server_scripts {
-    'server/config.lua',
-    'server/init.lua',
-    'server/modules/api.lua',
-    'server/esx_integration.lua',
-    'server/modules/file.lua'
+exports {
+    'getSharedObject',
+    'getServerObject'
 }
