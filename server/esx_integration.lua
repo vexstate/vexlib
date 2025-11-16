cc315ba47782afed9950244fac30dcbe3a2d60e6
м <174154109+n11kol11c@users.noreply.github.com>
2025-11-16 19:24:20 +0100

    Update esx_integration.lua

--- server/esx_integration.lua
+++ server/esx_integration.lua
@@ -1,9 +1,28 @@
+EVX = EVX or {}
+if EVX.isEnv("server") == -1 then return end
+
 ESX = exports['es_extended']:getSharedObject()
-local promise = 0
 
 Citizen.CreateThread(function ()
     while ESX == nil do
         ESX = exports['es_extended']:getSharedObject()
-        Citizen.Wait(promise)
+        Citizen.Wait(EVX.setpromise(0))
     end
 end)
+
+if ESX then
+    TriggerClientEvent('chat:addMessage', source, {
+        color = {200, 100, 100},
+        args = {"vexlib", ("ESX loaded successfully: %s"):format(ESX and 'detected' or 'unknown')}
+    })
+end
+
+if exports.ox_inventory then
+    TriggerClientEvent('chat:addMessage', source, {
+        color = {200, 100, 100},
+        args = {"vexlib",
+        ("OX loaded successfully: %s version"):format(
+        exports.ox_inventory and 'detected' or 'unknown'
+    )}
+    })
+end
