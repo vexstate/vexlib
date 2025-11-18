ebb9949fdfa511fe6f04defa5dcec2e4ff7b4dce
м <174154109+n11kol11c@users.noreply.github.com>
2025-11-18 20:49:05 +0100

    Update esx_integration.lua

--- server/esx_integration.lua
+++ server/esx_integration.lua
@@ -1,28 +1,22 @@
-EVX = EVX or {}
-if EVX.isEnv("server") == -1 then return end
-
 ESX = exports['es_extended']:getSharedObject()
 
-Citizen.CreateThread(function ()
-    while ESX == nil do
-        ESX = exports['es_extended']:getSharedObject()
-        Citizen.Wait(EVX.setpromise(0))
+local hasESX = ESX ~= nil
+local hasOX = exports.ox_inventory ~= nil
+
+AddEventHandler('playerSpawned', function(spawn)
+    local src = source
+    local player = ESX.GetPlayerFromId(src)
+    local _crg = GetPlayerGroup(player)
+
+    if _crg == 'admin' or _crg == 'superadmin' then
+        TriggerClientEvent('chat:addMessage', src, {
+            color = {200, 100, 100},
+            args = {"vexlib", ("ESX loaded successfully: %s"):format(hasESX and 'detected' or 'unknown')}
+        })
+
+        TriggerClientEvent('chat:addMessage', src, {
+            color = {200, 100, 100},
+            args = {"vexlib", ("OX Inventory loaded successfully: %s"):format(hasOX and 'detected' or 'unknown')}
+        })
     end
 end)
-
-if ESX then
-    TriggerClientEvent('chat:addMessage', source, {
-        color = {200, 100, 100},
-        args = {"vexlib", ("ESX loaded successfully: %s"):format(ESX and 'detected' or 'unknown')}
-    })
-end
-
-if exports.ox_inventory then
-    TriggerClientEvent('chat:addMessage', source, {
-        color = {200, 100, 100},
-        args = {"vexlib",
-        ("OX loaded successfully: %s version"):format(
-        exports.ox_inventory and 'detected' or 'unknown'
-    )}
-    })
-end
