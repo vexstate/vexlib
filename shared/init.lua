62d65a7a0bdffbce4212dd93052fd3fb09afc805
м <174154109+n11kol11c@users.noreply.github.com>
2025-11-18 20:48:06 +0100

    Update init.lua

--- shared/init.lua
+++ shared/init.lua
@@ -1,17 +1,12 @@
 EVX = EVX or {}
 EVX.env = EVX.env or (IsDuplicityVersion() and 'server' or 'client')
 
-EVX.meta = { version = "3.0.0", author = "Matija" }
+EVX.meta = { version = "3.0.0", author = "Matija", _github = "https://github.com/vexstate"}
 
 if EVX.env == 'server' then
     EVX._modules = EVX._modules or {}
     EVX._exports = EVX._exports or {}
     EVX._functions = EVX._functions or {}
-
-    EVX._exports = setmetatable({}, {
-        __newindex = function()
-            error("Cannot modify exports directly!")
-        end})
 
     function EVX.registerExport(name, fn)
         if type(name) ~= 'string' or type(fn) ~= 'function' then return end
@@ -27,14 +22,6 @@ function EVX.isEnv(env)
     else return 0 end
 end
 
-function EVX.getExport(name)
-    if EVX._exports then
-       return EVX._exports[name]
-    end
-end
-
-function EVX.getModule(name)
-    if EVX._modules then
-       return EVX._modules[name]
-    end
-end
+EVX.registerExport('getSharedObject', function ()
+    return EVX
+end)
