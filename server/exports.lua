2c0aa71c496f0880a7a00b86d34fb2793182b64d
м <174154109+n11kol11c@users.noreply.github.com>
2025-11-20 23:01:07 +0100

    Update exports.lua

--- server/exports.lua
+++ server/exports.lua
@@ -1,4 +1,5 @@
 if EVX.env ~= "server" then return end
+if EVX.isEnv("server") == -1 then return end
 
 EVX._exports = EVX._exports or {}
 
@@ -6,10 +7,15 @@ function EVX.registerExport(name, fn)
     if type(name) ~= "string" or type(fn) ~= "function" then return end
     if EVX._exports[name] then return end
 
+    if not exports then
+        error("[vexlib:server/exports] No export module found.", 2)
+        return nil
+    end
+
     EVX._exports[name] = fn
     exports(name, fn)
 end
 
-EVX.registerExport("getServerCore", function()
+EVX.registerExport("server", function()
     return EVX
 end)
