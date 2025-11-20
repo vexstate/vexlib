b92cf412fda4d8bd3167c9fac7808a6531268b70
м <174154109+n11kol11c@users.noreply.github.com>
2025-11-20 23:02:45 +0100

    Update exports.lua

--- client/exports.lua
+++ client/exports.lua
@@ -1,19 +1,20 @@
 if EVX.env ~= "client" then return end
+if EVX.isEnv("client") == -1 then return end
 
-exports("getUtils", function()
+exports("utils_get", function()
     return EVX.utils
 end)
 
-exports("getLocale", function()
+exports("locale_get", function()
     return {
         get = EVX.Locale.get
     }
 end)
 
-exports("getFormat", function()
+exports("format_get", function()
     return EVX.format
 end)
 
-exports("getString", function()
+exports("string_get", function()
     return EVX.string
 end)
