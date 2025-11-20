7e5ec639edce542ad31ac1864644f136cf081231
м <174154109+n11kol11c@users.noreply.github.com>
2025-11-20 14:12:11 +0100

    Delete server directory

--- server/commands.lua
+++
@@ -1,9 +0,0 @@
-EVX = EVX or {}
-
-EVX.registerCommand('vexlib:version', function ()
-    TriggerClientEvent('vexlib:shownotif', source, ("[vexlib] Version: %s"):format(EVX.meta.version))
-end, false)
-
-EVX.registerCommand('vexlib:author', function ()
-    TriggerClientEvent('vexlib:shownotif', source, ("[vexlib] Author: %s"):format(EVX.meta.author))
-end, false)
